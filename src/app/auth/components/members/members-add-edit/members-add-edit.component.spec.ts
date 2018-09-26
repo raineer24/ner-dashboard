982a61607104edc10d78b738f6d18ba4d4c436d8
raineer <delaritarainer81@gmail.com>
2018-09-26 16:49:10 +0800

    Add members-add-edit

---
+++ src/app/auth/components/members/members-add-edit/members-add-edit.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MembersAddEditComponent } from './members-add-edit.component';
+
+describe('MembersAddEditComponent', () => {
+  let component: MembersAddEditComponent;
+  let fixture: ComponentFixture<MembersAddEditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MembersAddEditComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MembersAddEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
