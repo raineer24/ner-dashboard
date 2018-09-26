d4ec133fa5c1f40ed9cbefeade7191a3fb326044
raineer <delaritarainer81@gmail.com>
2018-09-26 17:13:44 +0800

    formcontrol name

--- src/app/auth/components/members/members-add-edit/members-add-edit.component.ts
+++ src/app/auth/components/members/members-add-edit/members-add-edit.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-members-add-edit',
   templateUrl: './members-add-edit.component.html',
   styleUrls: ['./members-add-edit.component.scss']
 })
 export class MembersAddEditComponent implements OnInit {
+  addEditMemberForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  initForm(): void {
+    this.addEditMemberForm = this.fb.group({
+      username: ['', Validators.required]
+    })
+  }
 }
